9ed50f57052185abef7529764444a884f4673bd4
Dima Chachin <chachin99@bk.ru>
2022-12-01 19:18:12 +0300

    Третье задание

---
+++ Exemple_3/Program.cs
@@ -0,0 +1,35 @@
+﻿//функция получения числа с консоли
+int GetNumber(string message)
+{
+int result = 0;
+bool isCorrect = false;
+
+
+while(!isCorrect)
+{
+Console.WriteLine(message);
+
+if(int.TryParse(Console.ReadLine(), out result) && result != 0)
+{
+isCorrect = true;
+}
+else
+{
+Console.WriteLine("Ввели не число или 0. Введите корректное число");
+}
+}
+
+return result;
+}
+
+int n = GetNumber("Ведите N:");
+
+for (int i = 1; i < n; i++)
+{
+    int sum = i * i * i;
+    Console.Write($"{sum},");
+}
+
+int result = n * n * n;
+
+Console.Write($"{result}.");
